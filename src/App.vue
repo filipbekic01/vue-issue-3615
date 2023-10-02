be8f65bc99963e1412bd0e230535a37df24cf7f6
Filip Bekic <filip1994sm@gmail.com>
2023-10-02 13:26:06 +0200

    Init files

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const name = ref('test');
+</script>
+
+<template>
+ <MyInput :model-value="name" @update:model-value="(e) => name = e"/>
+</template>
---
+++ src/components/MyInput.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts" generic="T extends string | undefined | null">
+defineProps<{
+  modelValue: T;
+}>();
+
+const emit = defineEmits<{
+  (e: "update:model-value", modelValue: T): void;
+}>();
+</script>
+
+<template>
+  <input
+    type="text"
+    :model-value="modelValue"
+    @input="(e) => emit('update:model-value', e)"
+  />
+</template>
