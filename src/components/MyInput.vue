595b371f441e9f966343c7efe735b146607cc0d2
Filip Bekic <filip1994sm@gmail.com>
2023-10-02 14:07:38 +0200

    Update

--- src/components/MyInput.vue
+++ src/components/MyInput.vue
@@ -1,17 +1,20 @@
 <template>
-  <input
-    type="text"
-    :value="modelValue"
-    @input="(e) =>emit('update:model-value', ( e.target as HTMLInputElement).value as T)"
-  />
+  <input type="text" v-model="localModel" />
 </template>
 
-<script setup lang="ts" generic="T extends string">
-defineProps<{
+<script setup lang="ts" generic="T extends string | number">
+import { computed } from "vue";
+
+const props = defineProps<{
   modelValue: T;
 }>();
 
 const emit = defineEmits<{
   (e: "update:model-value", value: T): void;
 }>();
+
+const localModel = computed({
+  get: () => props.modelValue,
+  set: (value: T) => emit("update:model-value", value),
+});
 </script>
